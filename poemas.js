c1ff697d39c6d765e1baa2661452a4a31a8cb1b9
Amor <peliculasvaleryoscar@gmail.com>
2024-12-05 23:11:54 -0500

    Update poemas.js

--- poemas.js
+++ poemas.js
@@ -15,6 +15,6 @@ const auth = getAuth(app);
 
 onAuthStateChanged(auth, (user) => {
     if (!user) {
-        window.location.href = '/index.html';
+        window.location.href = 'index.html';
     }
 });
